ff6771da4af5ca62ea86aa148062a6c497ed4aa6
Jacobo B. <nireiorange@gmx.com>
2025-01-02 20:41:45 +0100

    chore: configure ruff (#18)
    We configure ruff as the project's formatter.

--- run_tests.py
+++ run_tests.py
@@ -1,24 +1,42 @@
 import os
 import subprocess
 
+GREEN = "\033[32m"
+RED = "\033[31m"
+RESET = "\033[0m"
+
 
 def run_tests():
     failed_result = False
-    test_directory = 'tests'
-    
+    test_directory = "tests"
+    errors = []
+
     for file in os.listdir(test_directory):
-        if file.endswith('.imp'):
+        if file.endswith(".imp"):
             test_path = os.path.join(test_directory, file)
-            print(f"Running test {test_path}")
+            print(f"Running test {test_path}", end=" ")
             try:
-                subprocess.run(['./imperivm', test_path], check=True, text=True, capture_output=True)
-                print("\033[32mOK\033[0m")
+                subprocess.run(["./imperivm", test_path], check=True, text=True, capture_output=True)
+                print(f"{GREEN}OK{RESET}")
             except subprocess.CalledProcessError as e:
-                print("\033[31mKO\033[0m")
-                print(f"Error: {e.stderr}")
+                print(f"{RED}KO{RESET}")
+                errors.append((test_path, e.stderr))
                 failed_result = True
-    
+
+    if errors:
+        print("\nErrors:")
+    for test, error in errors:
+        print(f"On {test}")
+        print(f"{RED}{error}{RESET}")
+        print()
+
+    if errors:
+        print("Failed tests:")
+    for test, _ in errors:
+        print(f"\t{RED}{test}{RESET}")
+
     return failed_result
+
 
 if __name__ == "__main__":
     failed = run_tests()
