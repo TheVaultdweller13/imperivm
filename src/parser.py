8371335f87e46db205f697221650815b7beeb14f
Héctor Fernández Hompanera <geralt741@gmail.com>
2025-01-02 21:05:18 +0100

    chore: Remove parser main

--- src/parser.py
+++ src/parser.py
@@ -1,10 +1,6 @@
-#!/bin/env python
-import argparse
-import pprint
-
 import grammar
-import visitor
 import preprocessor
+import visitor
 
 
 class ImperivmParser:
@@ -22,15 +18,3 @@ class ImperivmParser:
         program = self.preprocessor.process(program)
         tree = self.grammar.parse(program)
         return self.visitor.visit(tree)
-
-
-if __name__ == "__main__":
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("filename", nargs=1)
-    args = argparser.parse_args()
-    data = None
-    with open(args.filename[0], "r") as file:
-        code = file.read()
-
-    ast = ImperivmParser().parse(code)
-    pprint.pprint(ast)
